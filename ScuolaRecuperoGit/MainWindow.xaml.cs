45fcb9fea8f93993d8577323e3e872a38ced6ea4
Mose Barbieri <MOSE.BARBIERI@local.ittcesena.it>
2023-03-16 09:48:09 +0100

    ultimazione delle varie operazioni

--- ScuolaRecuperoGit/MainWindow.xaml.cs
+++ ScuolaRecuperoGit/MainWindow.xaml.cs
@@ -29,5 +29,40 @@ namespace ScuolaRecuperoGit
         {
 
         }
+
+        private void btnCercaAlunno_Click(object sender, RoutedEventArgs e)
+        {
+            Alunno a = new Alunno();
+
+           int eta =  Convert.ToInt32( txboxEtàCercare.Text);
+
+            for (int i = 0; i < Alunni.lenght; i++)
+            {
+                if (eta == a.età)
+                {
+                    studente_trovato.Text = a.Nome; 
+                }
+            }
+        }
+
+        private void btnAggiungiAlunno_Click(object sender, RoutedEventArgs e)
+        {
+            Alunno a = new Alunno();
+
+            a.Nome = txboxNomeAlunno.Text;
+            a.Cognome = txboxCognomeAlunno.Text;
+            a.Età = Convert.ToInt32(txboxetàAlunno.Text);
+            a.Città = txboxCittàAlunno.Text;
+
+            //aggiungere l'allunno alla lista degli studenti quando sarà disponibile
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Alunno a = new Alunno();
+            a.Classi();
+
+        }
     }
 }
