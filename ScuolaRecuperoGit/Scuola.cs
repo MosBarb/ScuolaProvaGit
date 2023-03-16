1c1120005181b31115ec28027d74068c91e45560
Mose Barbieri <MOSE.BARBIERI@local.ittcesena.it>
2023-03-16 09:52:57 +0100

    ultima versione

---
+++ ScuolaRecuperoGit/Scuola.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScuolaRecuperoGit
+{
+    internal class Scuola
+    {
+        public string NomeScuola { get; set; }
+        public int OrarioApertura { get; set; }
+        public int OrarioChiusura { get; set; }
+        public List<Alunno> Alunni { get; set; } // Alunno sarà visto quando faremo il merge
+    }
+}
