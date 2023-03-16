45b097dd96bb5b4366a41018fbc33e8f51ecb517
Mose Barbieri <MOSE.BARBIERI@local.ittcesena.it>
2023-03-16 09:19:26 +0100

    aggiunta la funzione della classe Classi e creata lista studenti

--- ScuolaRecuperoGit/MainWindow.xaml.cs
+++ ScuolaRecuperoGit/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ScuolaRecuperoGit
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
